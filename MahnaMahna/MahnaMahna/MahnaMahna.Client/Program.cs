016ab3df51c6538a453731bc2c73975122c1364c
Boramere <meade.benjamin@gmail.com>
2024-11-10 15:34:51 +0800

    Final implementation

--- MahnaMahna/MahnaMahna/MahnaMahna.Client/Program.cs
+++ MahnaMahna/MahnaMahna/MahnaMahna.Client/Program.cs
@@ -1,4 +1,5 @@
 using MahnaMahna.Client.Services;
+using MahnaMahna.Shared.Services;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -6,5 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 //Services
 builder.Services.AddScoped<ITodoApiService, TodoApiService>();
 builder.Services.AddHttpClient("", (HttpClient client) => { client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress); });
+
+//SignalR
+builder.Services.AddScoped<INotificationService, NotificationService>();
 
 await builder.Build().RunAsync();
